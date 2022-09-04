2f676ca6aba48d6523d65ec94ccc9b7d07b4d116
celdyner <h.oranekwulu@gmail.com>
2022-09-04 12:02:31 +0100

    first commit

--- src/components/pages/notFound.js
+++ src/components/pages/notFound.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <h1 style={{marginTop: "5rem"}}>Page Not Found </h1>
+    <h1 style={{marginTop: "6rem"}}>Page Not Found </h1>
   )
 }
 
